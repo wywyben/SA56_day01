85bc68c67468fe6a485e596379fd130c04136ed3
Junwei <wywyben1999@gmail.com>
2023-06-08 15:36:22 +0800

    test

---
+++ src/test/java/sg/edu/nus/iss/SA56_d1/model/Employee.java
@@ -0,0 +1,16 @@
+package sg.edu.nus.iss.SA56_d1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class Employee {
+    
+    private String id;
+    private String firstName;
+    private String lastName;
+
+}
