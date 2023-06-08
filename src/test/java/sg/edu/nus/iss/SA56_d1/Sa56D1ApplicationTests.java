fd7eed2b6564c78214e94f23a944d51f1eef60e9
Junwei <wywyben1999@gmail.com>
2023-06-08 14:59:52 +0800

    my 1st commit

---
+++ src/test/java/sg/edu/nus/iss/SA56_d1/Sa56D1ApplicationTests.java
@@ -0,0 +1,13 @@
+package sg.edu.nus.iss.SA56_d1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sa56D1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
